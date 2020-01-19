67d6d0862784d6b3ea5fb6b09718c9ef624308bd
marinrusu1997 <dimarusu2000@gmail.com>
2020-01-19 16:15:16 +0200

    XSS sanitization

--- lib.firewall/lib/index.ts
+++ lib.firewall/lib/index.ts
@@ -1,7 +1,8 @@
-import { services, fs } from '@marin/lib.utils';
+import { services, fs, misc } from '@marin/lib.utils';
 import { readdir } from 'fs';
 import { promisify } from 'util';
 import Ajv from 'ajv';
+import { FilterXSS } from 'xss';
 
 const readDir = promisify(readdir);
 
@@ -9,9 +10,12 @@ const SCHEMAS_DIR = `${__dirname}/schemas`;
 const EXCLUDE_SCHEMAS_DIR = ['core'];
 
 class Firewall {
+	// eslint-disable-next-line @typescript-eslint/explicit-function-return-type
 	private static readonly validator: Ajv.Ajv = new Ajv({ loadSchema: uri => fs.readJsonFromFile(`${SCHEMAS_DIR}/${uri}`) });
 
-	static init(): Promise<void> {
+	private static readonly xssFilter: FilterXSS = new FilterXSS();
+
+	public static init(): Promise<void> {
 		return readDir(SCHEMAS_DIR)
 			.then(schemasDirs => {
 				return schemasDirs.filter(schemaDir => !EXCLUDE_SCHEMAS_DIR.includes(schemaDir));
@@ -25,7 +29,9 @@ class Firewall {
 							for (let j = 0; j < schemas.length; j++) {
 								schemasPromises.push(
 									fs.readJsonFromFile(`${SCHEMAS_DIR}/${servicesSchemasDirs[i]}/${schemas[j]}`).then(schema => {
+										// eslint-disable-next-line @typescript-eslint/ban-ts-ignore
 										// @ts-ignore
+										// eslint-disable-next-line no-param-reassign
 										schema.$async = true;
 										Firewall.validator.addSchema(schema);
 										return Firewall.validator.compileAsync(schema);
@@ -40,13 +46,16 @@ class Firewall {
 			});
 	}
 
-	static validate(service: services.SERVICES, method: string, data: object): Promise<object> {
+	public static validate(service: services.SERVICES, method: string, data: object): Promise<object> {
 		const schemaId = Firewall.computeSchemaId(service, method);
 		return Firewall.validator.validate(schemaId, data) as Promise<object>;
 	}
 
-	static sanitize(data: object): void {
-		console.warn(data);
+	public static sanitize(data: object | string): object | string {
+		if (typeof data === 'string') {
+			return Firewall.xssFilter.process(data);
+		}
+		return misc.traverse(data, value => (typeof value === 'string' ? Firewall.xssFilter.process(value) : undefined));
 	}
 
 	private static computeSchemaId(service: services.SERVICES, method: string): string {
--- lib.utils/lib/misc.ts
+++ lib.utils/lib/misc.ts
@@ -21,4 +21,55 @@ function isEmptyObject(obj: object): boolean {
 	return Object.entries(obj).length === 0 && obj.constructor === Object;
 }
 
-export { clone, cloneDeep, removeItemFromArray, isEmptyObject };
+type TraverseProcessor = (value: boolean | number | string) => void | boolean | number | string;
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+function traverse(object: object, processor: TraverseProcessor, alterDeepClone?: boolean): object {
+	if (alterDeepClone) {
+		// eslint-disable-next-line no-param-reassign
+		object = cloneDeep(object);
+	}
+
+	// eslint-disable-next-line @typescript-eslint/no-explicit-any
+	function traverseArray(arr: Array<any>): void {
+		for (let i = 0; i < arr.length; i++) {
+			// eslint-disable-next-line no-use-before-define, @typescript-eslint/no-use-before-define
+			continueTraversal(arr, i, arr[i]);
+		}
+	}
+
+	function traverseObject(obj: object): void {
+		const keys = Object.getOwnPropertyNames(obj);
+		for (let i = 0; i < keys.length; i++) {
+			const key = keys[i];
+			// eslint-disable-next-line @typescript-eslint/ban-ts-ignore
+			// @ts-ignore
+			// eslint-disable-next-line no-use-before-define, @typescript-eslint/no-use-before-define
+			continueTraversal(obj, key, obj[key]);
+		}
+	}
+
+	// eslint-disable-next-line @typescript-eslint/no-explicit-any
+	function continueTraversal(currentObject: object | Array<any>, key: string | number, value: any): void {
+		if (Array.isArray(value)) {
+			traverseArray(value);
+		} else if (typeof value === 'object' && value !== null) {
+			traverseObject(value);
+		} else if (typeof value === 'boolean' || typeof value === 'number' || typeof value === 'string') {
+			const processedVal = processor(value);
+			if (processedVal) {
+				// eslint-disable-next-line @typescript-eslint/ban-ts-ignore
+				// @ts-ignore
+				// eslint-disable-next-line no-param-reassign
+				currentObject[key] = processedVal;
+			}
+		}
+	}
+
+	traverseObject(object);
+
+	return object;
+}
+
+// eslint-disable-next-line no-undef
+export { clone, cloneDeep, removeItemFromArray, isEmptyObject, traverse, TraverseProcessor };
