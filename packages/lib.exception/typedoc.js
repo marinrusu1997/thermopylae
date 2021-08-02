f9e9ce642214e0bf9f5e9583688b68b8c7cc3107
Rusu Marin <dimarusu2000@gmail.com>
2021-08-02 11:26:53 +0300

    lib.exception api doc

--- packages/lib.exception/typedoc.js
+++ packages/lib.exception/typedoc.js
@@ -1,1 +1,8 @@
-module.exports = require('@thermopylae/dev.environment').configs.typedoc;
+const typedoc = require('@thermopylae/dev.environment').configs.typedoc;
+
+typedoc.links = [{
+    "label": "Github",
+    "url": "https://github.com/marinrusu1997/thermopylae/tree/master/packages/lib.exception"
+}];
+
+module.exports = typedoc;
